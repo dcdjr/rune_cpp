356869bb537f44ee309c9a92b22cdb133a028245
dcdjr <ddipietro8227@gmail.com>
2026-07-31 16:29:10 -0400

    Implement token.hpp header with TokenKind enum and Token struct

---
+++ include/rune/token.hpp
@@ -0,0 +1,35 @@
+#ifndef RUNE_TOKEN_HPP
+#define RUNE_TOKEN_HPP
+
+#include <cstddef>
+#include <string_view>
+
+namespace rune {
+    enum class TokenKind {
+        TOK_PRINT,
+
+        TOK_INT,
+
+        TOK_SEMICOLON,
+
+        TOK_PLUS,
+        TOK_MINUS,
+        TOK_STAR,
+        TOK_SLASH,
+
+        TOK_LPAREN,
+        TOK_RPAREN,
+
+        TOK_EOF
+    };
+
+    struct Token {
+        TokenKind kind;
+        std::string_view lexeme;
+        std::size_t line;
+        std::size_t column;
+    };
+}
+
+
+#endif

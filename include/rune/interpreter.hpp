85d1dbbab125cc527020418d8a566643fd41ef09
dcdjr <ddipietro8227@gmail.com>
2026-08-14 16:32:02 -0400

    Add first runtime environment

--- include/rune/interpreter.hpp
+++ include/rune/interpreter.hpp
@@ -2,11 +2,16 @@
 #define RUNE_INTERPRETER_HPP
 
 #include "ast.hpp"
+#include <unordered_map>
+#include <string>
 
 namespace rune 
 {
 
 class Interpreter {
+private:
+    std::unordered_map<std::string, int> environment_;
+
 public:
     int evaluate(const Expr& expr);
 };

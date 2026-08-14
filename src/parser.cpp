24303d7feb3871c1d2b4bd422194cdf1de6b826d
dcdjr <ddipietro8227@gmail.com>
2026-08-14 14:54:29 -0400

    Add statement and program parsing

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <stdexcept>
+#include <vector>
 
 namespace rune 
 {
@@ -155,7 +156,7 @@ std::unique_ptr<Stmt> Parser::parse_statement() {
 std::vector<std::unique_ptr<Stmt>> Parser::parse_program() {
     std::vector<std::unique_ptr<Stmt>> statements;
 
-    while (!check(TokenKind::TOK_EOF))
+    while (!is_at_end())
         statements.push_back(parse_statement());
 
     return statements;
